76448a2a10de520173c4a1c2f770cb9d0559a68a
Dvelosho <dvelosocastillo@gmail.com>
2025-02-23 17:07:05 -0300

    Initial Commit

---
+++ fireBaseaouthdemo/fireBaseaouthdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  fireBaseaouthdemoApp.swift
+//  fireBaseaouthdemo
+//
+//  Created by daniel veloso on 23-02-25.
+//
+
+import SwiftUI
+
+@main
+struct fireBaseaouthdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
