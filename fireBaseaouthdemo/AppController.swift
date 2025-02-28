65ed3e4089ceeea54f963afbd81c59ca1d486c87
Dvelosho <dvelosocastillo@gmail.com>
2025-02-28 11:04:19 -0300

    [ADD] cambiar password borrar usuario

--- fireBaseaouthdemo/AppController.swift
+++ fireBaseaouthdemo/AppController.swift
@@ -37,4 +37,72 @@ class AppController{
     func signOut() throws {
         try Auth.auth().signOut()
     }
+    
+    func getUserProfile() {
+        let user = Auth.auth().currentUser
+        
+        if let user {
+            let uid = user.uid
+            let email = user.email
+            let photoURL = user.photoURL
+        }
+    }
+    
+    func updateUserProfile() {
+        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+        changeRequest?.displayName = "Ada"
+        changeRequest?.commitChanges(completion: { err in
+            if let err {
+                print(err.localizedDescription)
+            }
+            
+        })
+    }
+    
+    func setUserEmail() {
+        Auth.auth().currentUser?.updateEmail(to: <#T##String#>, completion: { err in
+            if let err {
+                print(err.localizedDescription)
+            }
+        })
+    }
+    
+    func reAuthenticate() {
+        let user = Auth.auth().currentUser
+        let credential = EmailAuthProvider.credential(withEmail: email, password: password)
+        
+        user?.reauthenticate(with: credential, completion: { result, err in
+            if let err {
+                print(err)
+            }
+            print(result)
+        })
+    }
+    
+    func sendUserEmailVerification() {
+        Auth.auth().currentUser?.sendEmailVerification(completion: { err in
+            if let err {
+                print(err.localizedDescription)
+            }
+        })
+    }
+    
+    func setUserPassword() {
+        Auth.auth().currentUser?.updatePassword(to: <#T##String#>, completion: { err in
+            if let err {
+                print(err.localizedDescription)
+            }
+        })
+    }
+    
+    func deleteUser() {
+        
+        let user = Auth.auth().currentUser
+        
+        user?.delete(completion: { err in
+            if let err {
+                print(err.localizedDescription)
+            }
+        })
+    }
 }
